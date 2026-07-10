64d7eb94723e62f81d6273f4892ba87b90b87a4e
GANTA BALA AMOGH RAJ <balaamoghraj@gmail.com>
2026-07-10 12:15:46 +0530

    Time: 10 ms (87.15%), Space: 22.6 MB (40.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = {}
+        for word in strs:
+            sorted_word = tuple(sorted(word))
+            anagram_map.setdefault(sorted_word, []).append(word)
+        return list(anagram_map.values())
