8598b3b7bf218675bab510aa20acde1b464da53b
GANTA BALA AMOGH RAJ <balaamoghraj@gmail.com>
2026-07-15 14:29:32 +0530

    Time: 7 ms (57.52%), Space: 21.3 MB (19.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         newset = list(set(nums))
-        o = len(nums)/2
         for i in range(len(newset)):
-            if nums.count(newset[i]) > o:
+            if nums.count(newset[i]) > len(nums) / 2:
                 return newset[i]
